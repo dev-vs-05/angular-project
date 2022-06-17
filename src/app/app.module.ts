91800b7593e893e5ed11d2263a2f0ec5a1166128
Vinayak S <vinayak@github.com>
2022-06-18 00:36:38 +0530

    app-module-file-push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AngularBasicsComponent } from './angular-basics/angular-basics.component';
 import { FormsModule } from '@angular/forms';
 import { AngularDirectivesComponent } from './angular-directives/angular-directives.component';
+import { AngularComponentsAndDatabindingComponent } from './angular-components-and-databinding/angular-components-and-databinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AngularBasicsComponent,
     AngularDirectivesComponent,
+    AngularComponentsAndDatabindingComponent,
   ],
   imports: [
     BrowserModule,
